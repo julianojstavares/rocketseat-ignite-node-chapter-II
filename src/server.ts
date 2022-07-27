82a46b24d9038cf2310ce2b2c67122148b4517e7
julianojstavares <julianojstavares@outlook.com>
2022-07-27 16:30:06 -0300

    Updating

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,18 @@ const port = process.env.PORT
 
 const app = express();
 
-app.get("/", (request, response) => {
+app.use(express.json());
+
+app.get("/", (request, response) =>
+{
     return response.json({ message: "Hello World" });
+})
+
+app.post("/courses", (request, response) =>
+{
+    const { name } = request.body;
+
+    return response.json({ name });
 })
 
 app.listen(port, () => console.log(`Server is running in ${host}:${port} `));
