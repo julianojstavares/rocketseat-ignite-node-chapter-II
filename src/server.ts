fe7722e17260105dec960dd649121af9183d41ee
julianojstavares <julianojstavares@outlook.com>
2022-07-27 15:52:48 -0300

    Updating message log in listen

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 import express from 'express';
+import 'dotenv/config';
+
+const host = process.env.HOST
+const port = process.env.PORT
 
 const app = express();
 
-app.listen(3333, () => console.log("Server is running"));
+app.listen(port, () => console.log(`Server is running in host "${host}" port "${port}" `));
