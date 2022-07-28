726c561a17597bcec7f20b1d291bc085b41983c1
julianojstavares <julianojstavares@outlook.com>
2022-07-28 15:11:31 -0300

    Working on categories routes

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,17 @@
 import { Router } from 'express';
-import { Category } from '../model/Category';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
 
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
     
     const { name, description } = request.body;
 
+    categoriesRepository.create({name, description});
 
-
-    return response.status(201).json({category});
+    return response.status(201).send();
 
 });
 
