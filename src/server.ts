40187c20ba98efcf580d5e014b0f11c7d2de8c05
julianojstavares <julianojstavares@outlook.com>
2022-07-28 11:43:00 -0300

    Adding categories routes

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'dotenv/config';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const host = process.env.HOST
 const port = process.env.PORT
@@ -8,16 +9,6 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (request, response) =>
-{
-    return response.json({ message: "Hello World" });
-})
-
-app.post("/courses", (request, response) =>
-{
-    const { name } = request.body;
-
-    return response.json({ name });
-})
+app.use(categoriesRoutes);
 
 app.listen(port, () => console.log(`Server is running in ${host}:${port} `));
