49ce41c7983affba8a6816f83274da8e4273742f
julianojstavares <julianojstavares@outlook.com>
2022-07-28 16:54:01 -0300

    Adding findByName method

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -34,7 +34,15 @@ class CategoriesRepository
     list(): Category[] {
         
         return this.categories;
-        
+
+    }
+
+    findByName(name: string): Category {
+
+        const category = this.categories.find(category => category.name === name);
+
+        return category;
+
     }
 
 }
