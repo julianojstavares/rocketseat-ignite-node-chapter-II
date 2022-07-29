eb58501c2e2aec7fcc29e78790100815420f0175
julianojstavares <julianojstavares@outlook.com>
2022-07-29 16:33:00 -0300

    Making specification service

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,27 @@
-class CreateSpecificationService {
+import { ISpecificationsRepository } from "../repositories/Specifications/ISpecificationsRepository";
 
-    execute() {
+interface IRequest
+{
+    name: string;
+    description: string;
+}
+
+class CreateSpecificationService
+{
+
+    constructor(private specificationsRepository: ISpecificationsRepository) {}
+
+    execute({ name, description }: IRequest): void
+    {
+
+        const specificationAlreadyExists = this.specificationsRepository.findByName(name);
+
+        if (specificationAlreadyExists)
+        {
+            throw new Error("Specification already exists!");
+        }
+
+        this.specificationsRepository.create({ name, description });
 
     }
 
