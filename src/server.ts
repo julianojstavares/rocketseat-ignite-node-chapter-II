4a6c54743ed53e976e0af22408c025156d4add31
julianojstavares <julianojstavares@outlook.com>
2022-07-29 16:33:18 -0300

    Creating specifications routes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
 import { categoriesRoutes } from './routes/categories.routes';
+import { specificationsRoutes } from './routes/specifications.routes';
 
 const host = process.env.HOST
 const port = process.env.PORT
@@ -10,5 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/categories", categoriesRoutes);
+
+app.use("/specifications", specificationsRoutes);
 
 app.listen(port, () => console.log(`Server is running in ${host}:${port} `));
