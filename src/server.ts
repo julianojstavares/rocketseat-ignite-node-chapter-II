55d0a44f60f8e191f12501fb6ac39a13eb6049b9
julianojstavares <julianojstavares@outlook.com>
2022-08-01 11:55:51 -0300

    Refactor routes/router

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import 'dotenv/config';
-import { categoriesRoutes } from './routes/categories.routes';
-import { specificationsRoutes } from './routes/specifications.routes';
+import { router } from './routes';
 
 const host = process.env.HOST
 const port = process.env.PORT
@@ -10,8 +9,6 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/categories", categoriesRoutes);
-
-app.use("/specifications", specificationsRoutes);
+app.use(router);
 
 app.listen(port, () => console.log(`Server is running in ${host}:${port} `));
