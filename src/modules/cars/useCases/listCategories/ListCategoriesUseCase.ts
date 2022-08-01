4d8378c9c8b75781870d46c75afdacf0a89b125b
julianojstavares <julianojstavares@outlook.com>
2022-08-01 10:38:15 -0300

    Implementing list categories use case

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -0,0 +1,26 @@
+import { Category } from "../../model/Category";
+import { ICategoriesRepository } from "../../repositories/Categories/ICategoriesRepository";
+
+interface IRequest
+{
+    name: string;
+    description: string;
+}
+
+class ListCategoriesUseCase {
+    
+    constructor(private categoriesRepository: ICategoriesRepository) {}
+
+    execute(): Category[]
+    {
+
+        const categories = this.categoriesRepository.list();
+
+        return categories;
+
+    }
+
+
+}
+
+export { ListCategoriesUseCase }
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express';
-import { CategoriesRepository } from '../modules/cars/repositories/Categories/CategoriesRepository';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
-
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
 
@@ -14,9 +12,7 @@ categoriesRoutes.post("/", (request, response) => {
 
 categoriesRoutes.get("/", (request, response) => {
 
-    const all = categoriesRepository.list();
-
-    return response.json(all);
+    return listCategoriesController.handle(request, response);
 
 });
 
