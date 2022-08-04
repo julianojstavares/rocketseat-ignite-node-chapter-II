43d4cd8b45cf2292c46c08f9f41ea23df02aa7d6
julianojstavares <julianojstavares@outlook.com>
2022-08-04 15:35:32 -0300

    Setup swagger

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import 'dotenv/config';
 import { router } from './routes';
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from "../swagger.json"
 
 const host = process.env.HOST
 const port = process.env.PORT
@@ -8,6 +10,8 @@ const port = process.env.PORT
 const app = express();
 
 app.use(express.json());
+
+app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
